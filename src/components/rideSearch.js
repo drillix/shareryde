8eb5902b5708cbcdb23f0040c6d0bd74f46e7e60
De-Great Yartey <yarteydegreat2@gmail.com>
2022-01-20 21:50:06 +0000

    Added routing

--- src/components/rideSearch.js
+++ src/components/rideSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {
   Container,
   Row,
@@ -10,11 +10,12 @@ import {
   Collapse,
 } from "react-bootstrap";
 import RangeSlider from "react-bootstrap-range-slider";
+import {HashRouter, NavLink, Route, Switch, useParams} from 'react-router-dom'
 
 const PageContent = () => {
   return (
     <div className="page">
-      <PageTabs />
+      <PageTabs/>
     </div>
   );
 };
@@ -22,10 +23,10 @@ const PageContent = () => {
 function FilterSearch() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(7);
-  const [ finalValue, setFinalValue ] = useState(null);
+  const [finalValue, setFinalValue] = useState(null);
 
   return (
-    
+
     <>
       <Button
         onClick={() => setOpen(!open)}
@@ -52,64 +53,65 @@ function FilterSearch() {
                   size="sm"
                   variant='warning'
                   min={0}
-                  step = {7}
+                  step={7}
                   max={252}
                   tooltipPlacement='top'
                   onAfterChange={e => setFinalValue(e.target.value)}
-                  
+
                 />
-                <p className="priceRange">Selected price limit per head: <span>GHS {finalValue}</span></p>
+                <p className="priceRange">Selected price limit per head: <span>GHS {finalValue}</span>
+                </p>
               </Col>
-              
+
             </Form.Group>
-            
+
           </Col>
 
-          <Col lg={8} className="px-lg-5" >
+          <Col lg={8} className="px-lg-5">
             <Row>
-            <Col lg={6} >
-            <Form.Group>
-              <div className="form-check form-switch">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id="highway"
-              />
-              <label className="form-check-label" htmlFor="highway">
-                Use Highway
-              </label>
-            </div>
-            <div className="form-check form-switch">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id="commercial"
-              />
-              <label className="form-check-label" htmlFor="commercial">
-                Commercial{" "}
-              </label>
-            </div>
-            
-              </Form.Group>
-            </Col>
-            <Col lg={6} >
-            <Form.Group>
-              
-            <div className="form-check form-switch">
-              <input className="form-check-input" type="checkbox" id="pets" />
-              <label className="form-check-label" htmlFor="pets">
-                Pets Allowed
-              </label>
-            </div>
-            <div className="form-check form-switch">
-              <input className="form-check-input" type="checkbox" id="women" />
-              <label className="form-check-label" htmlFor="women">
-                Bus Stops
-              </label>
-            </div>
-              </Form.Group>
-            </Col>
-              
+              <Col lg={6}>
+                <Form.Group>
+                  <div className="form-check form-switch">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="highway"
+                    />
+                    <label className="form-check-label" htmlFor="highway">
+                      Use Highway
+                    </label>
+                  </div>
+                  <div className="form-check form-switch">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="commercial"
+                    />
+                    <label className="form-check-label" htmlFor="commercial">
+                      Commercial{" "}
+                    </label>
+                  </div>
+
+                </Form.Group>
+              </Col>
+              <Col lg={6}>
+                <Form.Group>
+
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="pets"/>
+                    <label className="form-check-label" htmlFor="pets">
+                      Pets Allowed
+                    </label>
+                  </div>
+                  <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="women"/>
+                    <label className="form-check-label" htmlFor="women">
+                      Bus Stops
+                    </label>
+                  </div>
+                </Form.Group>
+              </Col>
+
             </Row>
           </Col>
         </Row>
@@ -125,14 +127,14 @@ const SearchForm = () => {
         <Col md>
           <Form.Group className="mb-1">
             <label>From:</label>
-            <Form.Control type="text" />
+            <Form.Control type="text"/>
           </Form.Group>
         </Col>
 
         <Col md>
           <Form.Group className="mb-1">
             <label>To:</label>
-            <Form.Control type="text" />
+            <Form.Control type="text"/>
           </Form.Group>
         </Col>
       </Row>
@@ -140,190 +142,207 @@ const SearchForm = () => {
         <Col md>
           <Form.Group className="mb-3">
             <label>Date:</label>
-            <Form.Control type="text" />
+            <Form.Control type="text"/>
           </Form.Group>
         </Col>
 
         <Col md>
           <Form.Group className="mb-3">
             <label className="Htext">.</label>
-            <Form.Control type="button" value="🔍 Search" />
+            <Form.Control type="button" value="🔍 Search"/>
           </Form.Group>
         </Col>
       </Row>
       <Row>
-        <FilterSearch />
+        <FilterSearch/>
       </Row>
     </Row>
   );
 };
 
 
+const RideItem = (props) => {
+  return (
+    <NavLink to={`/${props.id}`} >
+      <div className="rideItem" id={props.id}>
+        <img src={props.img} alt="img"/>
 
-const RideItem =(props)=>{
-  return(
-    <div className="rideItem" id = {props.id}>
-      <img src={props.img} alt="img" />
+        <div className="details">
+          <p className="Dname">{props.name}</p>
+          <p className="Tlocation">{props.location?.from} - {props.location?.to} </p>
+          <p className="Tduration">{props.duration?.departure} - {props.duration?.arrival}</p>
+        </div>
 
-      <div className="details">
-        <p className="Dname">{props.name}</p>
-        <p className="Tlocation">{props.location?.from} - {props.location?.to} </p>
-        <p className="Tduration">{props.duration?.departure} - {props.duration?.arrival}</p>
+        <div className="secondDetails">
+          <div className="price">{props.details?.price}<sup className="sup">GHS</sup></div>
+          <div className="seat">{props.details?.seatsAvailable} Seats</div>
+        </div>
+
+        <div className="specs">
+          <span>{props.addons.map(addon => (<i className="material-icons-outlined">{addon}</i>))}</span>
+        </div>
+
       </div>
-
-      <div className="secondDetails">
-      <div className="price">{props.details?.price}<sup className="sup">GHS</sup></div>
-      <div className="seat">{props.details?.seatsAvailable} Seats</div>
-      </div>
-     
-      <div className="specs">
-        <span>{props.addons.map(addon=>(<i className="material-icons-outlined">{addon}</i>))}</span>
-      </div> 
-
-    </div>
+    </NavLink>
   )
 
 }
 
 
+const Ride = () => {
 
-const Ride = () =>{
+  const Rides = [
+    {
+      id: 1,
+      img: "https://picsum.photos/60/60",
+      name: "Foster Asafo",
+      location: {
+        from: "Kumasi",
+        to: "Tamale",
+      },
+      duration: {
+        departure: "16:00am",
+        arrival: "3:00pm",
+      },
+      details: {
+        price: 120,
+        seatsAvailable: 5,
+      },
+      addons: ["wifi", "horizontal_distribute", "luggage"],
+    },
+    {
+      id: 2,
+      img: "https://picsum.photos/60/60",
+      name: "Kojo Oppong",
+      location: {
+        from: "Accra",
+        to: "Takoradi",
+      },
+      duration: {
+        departure: "6:00am",
+        arrival: "12:00pm",
+      },
+      details: {
+        price: 80,
+        seatsAvailable: 12,
+      },
+      addons: ["luggage", "smoke_free", "usb", "horizontal_distributed"],
+    },
+    {
+      id: 3,
+      img: "https://picsum.photos/60/60",
+      name: "Jeff Bezoz",
+      location: {
+        from: "Koforidua",
+        to: "Accra",
+      },
+      duration: {
+        departure: "3:00pm",
+        arrival: "7:00pm",
+      },
+      details: {
+        price: 25,
+        seatsAvailable: 10,
+      },
+      addons: ["pets", "smoke_free", "ac_unit"],
+    },
+    {
+      id: 4,
+      img: "https://picsum.photos/60/60",
+      name: "Brefo Gyamera",
+      location: {
+        from: "Nsawam",
+        to: "Accra",
+      },
+      duration: {
+        departure: "11:00am",
+        arrival: "2:00pm",
+      },
+      details: {
+        price: 10,
+        seatsAvailable: 15,
+      },
+      addons: ["stop_circle", "usb", "smoke_free"],
+    }
+  ];
 
-const Rides = [
-  {
-    id: 1,
-    img: "https://picsum.photos/60/60",
-    name: "Foster Asafo",
-    location: {
-      from: "Kumasi",
-      to: "Tamale",
-    },
-    duration: {
-      departure: "16:00am",
-      arrival: "3:00pm",
-    },
-    details: {
-      price: 120,
-      seatsAvailable: 5,
-    },
-    addons: ["wifi", "horizontal_distribute", "luggage"],
-  },
-  {
-    id: 2,
-    img: "https://picsum.photos/60/60",
-    name: "Kojo Oppong",
-    location: {
-      from: "Accra",
-      to: "Takoradi",
-    },
-    duration: {
-      departure: "6:00am",
-      arrival: "12:00pm",
-    },
-    details: {
-      price: 80,
-      seatsAvailable: 12,
-    },
-    addons: ["luggage", "smoke_free", "usb", "horizontal_distributed"],
-  },
-  {
-    id: 3,
-    img: "https://picsum.photos/60/60",
-    name: "Jeff Bezoz",
-    location: {
-      from: "Koforidua",
-      to: "Accra",
-    },
-    duration: {
-      departure: "3:00pm",
-      arrival: "7:00pm",
-    },
-    details: {
-      price: 25,
-      seatsAvailable: 10,
-    },
-    addons: ["pets", "smoke_free", "ac_unit"],
-  },
-  {
-    id: 4,
-    img: "https://picsum.photos/60/60",
-    name: "Brefo Gyamera",
-    location: {
-      from: "Nsawam",
-      to: "Accra",
-    },
-    duration: {
-      departure: "11:00am",
-      arrival: "2:00pm",
-    },
-    details: {
-      price: 10,
-      seatsAvailable: 15,
-    },
-    addons: ["stop_circle", "usb", "smoke_free"],
-  }
-];
-
-  return(
+  return (
     <>
-   {Rides.map(ride=>(
+      {Rides.map(ride => (
 
-    <RideItem 
-    id ={ride.id} 
-    img={ride.img} 
-    name={ride.name} 
-    location={ride.location} 
-    duration={ride.duration}
-    details={ride.details}
-    addons={ride.addons} 
-     />
+        <RideItem
+          id={ride.id}
+          img={ride.img}
+          name={ride.name}
+          location={ride.location}
+          duration={ride.duration}
+          details={ride.details}
+          addons={ride.addons}
+        />
 
-   ))}
-      
+      ))}
+
     </>
-   
+
   )
 }
-
 
 
 const RidesFeeds = () => {
   return (
-    <Col lg={7} >
-    <div className="rideHeaders">
-     <div className="date"><i className="material-icons-outlined">calendar_today</i>  Friday, 14 January 2022. </div>
-     <div className="location">Top Locations</div>
-    </div>
+    <Col lg={7}>
+      <div className="rideHeaders">
+        <div className="date"><i className="material-icons-outlined">calendar_today</i> Friday, 14 January 2022.
+        </div>
+        <div className="location">Top Locations</div>
+      </div>
 
-    <div className="rideContainer">
-      <Ride/>
-    </div>
-      
+      <div className="rideContainer">
+        <Ride/>
+      </div>
+
     </Col>
   );
 };
+
+function FeedContent() {
+  const {rideId} = useParams()
+
+
+  return (
+
+    <p>{rideId}</p>
+  )
+}
 
 const FeedView = () => {
 
 
   return (
     <Col className="feedView" lg={5}>
-        
-
+      <Route path='/:rideId'>
+        <FeedContent/>
+      </Route>
     </Col>
   );
 };
 
 
-
-
 const SearchRider = () => {
   return (
     <Container className="page-content">
-      <SearchForm />
+      <SearchForm/>
       <Row className="search-results">
-        <RidesFeeds />
-        <FeedView />
+        <HashRouter>
+          <Switch>
+            <Route path="/">
+              <>
+                <RidesFeeds/>
+                <FeedView/>
+              </>
+            </Route>
+          </Switch>
+        </HashRouter>
       </Row>
     </Container>
   );
@@ -342,10 +361,10 @@ const PageTabs = () => {
       className="mb-2 "
     >
       <Tab eventKey="riders" title="Drivers">
-        <SearchRider />
+        <SearchRider/>
       </Tab>
       <Tab eventKey="passengers" title="Passengers">
-        <SearchPassenger />
+        <SearchPassenger/>
       </Tab>
     </Tabs>
   );
--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -1,40 +1,53 @@
 import React from 'react';
 import logo from '../logo.svg'
-import {Link,useLocation} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 
-const Logo =()=>{
-     return(
-          <img className="logo" src={logo} alt="logo" />
-     )
+const Logo = () => {
+    return (
+        <img className="logo" src={logo} alt="logo"/>
+    )
 }
 
-const Menu = ()=>{
+const Menu = () => {
     const location = useLocation();
-    const { pathname } = location;
+    const {pathname} = location;
     const splitLocation = pathname.split("/");
 
-     return (
-        
-          <ul className="menu">
-             <Link to='/' ><li  className={splitLocation[1] === "" ? "activated" : "inactive"}><i className="material-icons">flip</i> Search</li></Link>
-             <Link to='/pages/account'><li  className={splitLocation[2] === "account" ? "activated" : "inactive"}><i className="material-icons-outlined">person</i> Accounts</li></Link>
-             <Link to='/pages/community'><li  className={splitLocation[2] === "community" ? "activated" : "inactive"}><i className="material-icons-outlined">forum</i> Community</li></Link>
-             <Link to='/pages/offer'><li className="menuBtn"><i className="material-icons">album</i> Offer Ride</li></Link>     
-          </ul>
-         
-        
-          
-     )
+    return (
+
+        <ul className="menu">
+            <Link to='/'>
+                <li className={splitLocation[1] === "" ? "activated" : "inactive"}><i
+                    className="material-icons">flip</i> Search
+                </li>
+            </Link>
+            <Link to='/pages/account'>
+                <li className={splitLocation[2] === "account" ? "activated" : "inactive"}><i
+                    className="material-icons-outlined">person</i> Accounts
+                </li>
+            </Link>
+            <Link to='/pages/community'>
+                <li className={splitLocation[2] === "community" ? "activated" : "inactive"}><i
+                    className="material-icons-outlined">forum</i> Community
+                </li>
+            </Link>
+            <Link to='/pages/offer'>
+                <li className="menuBtn"><i className="material-icons">album</i> Offer Ride</li>
+            </Link>
+        </ul>
+
+
+    )
 }
 
-const Sidemenu = ()=>{
-     return(
-          <div className="side-menu">
-               <Logo/>
-               <Menu/>
-          </div>
-     )
+const Sidemenu = () => {
+    return (
+        <div className="side-menu">
+            <Logo/>
+            <Menu/>
+        </div>
+    )
 }
 
 export default Sidemenu;
